a5a222945d9bc054da7a0540aeda6436b6e0924d
thomastyl <60270930+thomastyl@users.noreply.github.com>
2020-05-02 12:15:57 -0500

    added touch input example

--- Assets/JoyStickScript.cs
+++ Assets/JoyStickScript.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class JoyStickScript : MonoBehaviour
 {
+    Collider rightButtonCollider;
+    //Here you could declare a variable for the script on the right button
 
-    // Update is called once per frame
+     void Start()
+    {
+        rightButtonCollider = GameObject.Find("Right").GetComponent<Collider>();
+        //Here you could grab the script on the right button and assign it to a class variable
+    }
+
+
     void Update()
     {
-       
+        //This will detect if the user touched the right button.
+        //it won't actually move anything
+        //It is only setup for the right button
 
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 pos = touch.position;
+
+            if (rightButtonCollider.bounds.Contains(pos))
+            {
+
+                //here you could call the function of the script on the right button to move the character
+
+            }
+        }
+
+
+        }
     }
-}
 
 
 
